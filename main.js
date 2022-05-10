15fa630062d2d4c75ca5779654b03c7f7171d55c
reiniskom <reinis.komuls@gmail.com>
2022-05-10 16:49:28 +0300

    done2

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
 var select = document.getElementById("select");
-var arr = ["Latvian", "English", "Russian"];
+var arr = [
+    { id: 1, language: 'Latvian', title: 'Labdien!' },
+    { id: 2, language: 'English', title: 'Hello!' },
+    { id: 3, language: 'Russian', title: 'Добрый день!' }
+];
 
 for (var i = 0; i < arr.length; i++) {
     var option = document.createElement("OPTION");
-    var txt = document.createTextNode(arr[i]);
+    var txt = document.createTextNode(arr[i].language);
 
     option.appendChild(txt);
-    option.setAttribute("value", i);
+    option.setAttribute('value', arr[i].id);
     select.insertBefore(option, select.lastChild);
 }
 
+// add a change event listener to handle the language title
 select.addEventListener('change', changeHeading);
 
+// trigger a change event in order to display the selected language's title
+select.dispatchEvent(new Event('change'));
+
 function changeHeading() {
-    document.getElementById('heading').innerHTML = arr[select.value];
+    var language = arr.find((language) => language.id === parseInt(select.value));
+    document.getElementById('heading').innerHTML = language.title;
 }
